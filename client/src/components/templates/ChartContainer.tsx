9f4a9a03f158b67527b610a259b11087f22a66c8
Paras Jain <parasmani.jain2208@gmail.com>
2022-07-11 01:15:11 +0530

    categories chaart subtitle added

--- client/src/components/templates/ChartContainer.tsx
+++ client/src/components/templates/ChartContainer.tsx
@@ -12,29 +12,30 @@ const createChunks = (array, chunk) => {
   return temp;
 };
 
-export const ChartContainer = (props:any) => {
+export const ChartContainer = (props: any) => {
   const { title, fullHeight, apiData, stacked } = props;
   const [chartData, setChartData] = useState([]);
   const [width, height] = useWindowDimensions();
-  const chunkSize = width>= 1536 ? 50 : width>= 1200 ? 40 : width>= 900 ? 30 : width>=600 ? 20 : 10;
+  const chunkSize =
+    width >= 1536 ? 50 : width >= 1200 ? 40 : width >= 900 ? 30 : width >= 600 ? 20 : 10;
 
   const fetchData = () => {
     let chartDetails = [];
     if (apiData.length && title.toLowerCase().includes('universes')) {
-      const marvelUniverseData = apiData.filter((ele)=> (ele.name).toLowerCase().includes('marvel'));
-      const otherUniverseData = apiData.filter((ele)=> !(ele.name).toLowerCase().includes('marvel'));
+      const marvelUniverseData = apiData.filter((ele) => ele.name.toLowerCase().includes('marvel'));
+      const otherUniverseData = apiData.filter((ele) => !ele.name.toLowerCase().includes('marvel'));
       let data = [];
       let labels = [];
-      marvelUniverseData.forEach((element)=> {
-        data= [];
+      marvelUniverseData.forEach((element) => {
+        data = [];
         labels = [];
-        element.franchise.forEach((e)=> {
+        element.franchise.forEach((e) => {
           labels.push(e.name);
           data.push(e.length);
         });
         const obj = {
           subtitle: element.name,
-          width: width-50,
+          width: width - 50,
           labels,
           datasets: [
             {
@@ -43,19 +44,20 @@ export const ChartContainer = (props:any) => {
               hoverBackgroundColor: chartColors,
               data: data
             }
-          ] };
+          ]
+        };
         chartDetails = [...chartDetails, obj];
       });
-      otherUniverseData.forEach((element, i, arr)=> {
-        data= [];
+      otherUniverseData.forEach((element, i, arr) => {
+        data = [];
         labels = [];
-        element.franchise.forEach((e)=> {
+        element.franchise.forEach((e) => {
           labels.push(e.name);
           data.push(e.length);
         });
         const obj = {
           subtitle: element.name,
-          width: (width-50)/arr.length,
+          width: (width - 50) / arr.length,
           labels,
           datasets: [
             {
@@ -64,22 +66,25 @@ export const ChartContainer = (props:any) => {
               hoverBackgroundColor: chartColors,
               data: data
             }
-          ] };
+          ]
+        };
         chartDetails = [...chartDetails, obj];
       });
     } else if (apiData.length && title.toLowerCase().includes('categories')) {
       let data = [];
       let labels = [];
-      apiData.forEach((element)=> {
-        data= [];
+      apiData.forEach((element) => {
+        data = [];
         labels = [];
-        element.category.forEach((e)=> {
+        console.log(element);
+        element.category.forEach((e) => {
           labels.push(e.name);
           data.push(e.length);
         });
         const obj = {
           title: element.name,
-          width: width-50,
+          subtitle: element.name,
+          width: width - 50,
           labels,
           datasets: [
             {
@@ -88,26 +93,32 @@ export const ChartContainer = (props:any) => {
               hoverBackgroundColor: chartColors,
               data: data
             }
-          ] };
+          ]
+        };
         chartDetails = [...chartDetails, obj];
       });
     } else if (apiData.length && title.toLowerCase().includes('director movies')) {
-      const sets = createChunks(apiData, apiData.length> chunkSize ? Math.min(Math.ceil((apiData.length)/2), chunkSize): apiData.length);
-    
-      sets.forEach((e)=> {
+      const sets = createChunks(
+        apiData,
+        apiData.length > chunkSize
+          ? Math.min(Math.ceil(apiData.length / 2), chunkSize)
+          : apiData.length
+      );
+
+      sets.forEach((e) => {
         const data = [];
         const labels = [];
-        e.forEach((el)=> {
+        e.forEach((el) => {
           labels.push(el.movie_count);
-          data.push(el.director_count)
-        })
-        
+          data.push(el.director_count);
+        });
+
         const obj = {
           labels,
-          width: width-50,
+          width: width - 50,
           datasets: [
             {
-              label: "No of Directors",
+              label: 'No of Directors',
               backgroundColor: chartColors,
               hoverBackgroundColor: chartColors,
               data: data
@@ -117,18 +128,24 @@ export const ChartContainer = (props:any) => {
         chartDetails = [...chartDetails, obj];
       });
     } else {
-      const sets = createChunks(apiData, apiData.length> chunkSize ? Math.min(Math.ceil((apiData.length)/2), chunkSize): apiData.length);
-      sets.forEach((e)=> {
+      const sets = createChunks(
+        apiData,
+        apiData.length > chunkSize
+          ? Math.min(Math.ceil(apiData.length / 2), chunkSize)
+          : apiData.length
+      );
+      sets.forEach((e) => {
         const data = [];
         const labels = [];
-        e.forEach((e1)=> {
-          const label = e1.name + (title.toLowerCase().includes('movies') ? ' (' + e1.year + ')' : '');
+        e.forEach((e1) => {
+          const label =
+            e1.name + (title.toLowerCase().includes('movies') ? ' (' + e1.year + ')' : '');
           labels.push(label);
           data.push(e1.length);
         });
         const obj = {
           labels,
-          width: width-50,
+          width: width - 50,
           datasets: [
             {
               label: title.toLowerCase().includes('movies') ? 'Awards' : 'Movies',
@@ -152,11 +169,27 @@ export const ChartContainer = (props:any) => {
   }, [apiData]);
   return (
     <Box>
-      {[...chartData].length && [...chartData].map((data, index) =>
-        <RenderChart
-          key = {index} title = {title.toLowerCase().includes('universes')? data.subtitle : title} width = {data.width} data = {data}
-          index = {index} stacked={stacked}
-          canvasHeight = {(!fullHeight ? (chartData.length> 1 ? (height>500? height/2: height) : height): height/2)-50}/>)}
+      {[...chartData].length &&
+        [...chartData].map((data, index) => (
+          <RenderChart
+            key={index}
+            title={title.toLowerCase().includes('universes') ? data.subtitle : title}
+            width={data.width}
+            subtitle = {title.toLowerCase().includes('categories') ? data.subtitle : null}
+            data={data}
+            index={index}
+            stacked={stacked}
+            canvasHeight={
+              (!fullHeight
+                ? chartData.length > 1
+                  ? height > 500
+                    ? height / 2
+                    : height
+                  : height
+                : height / 2) - 50
+            }
+          />
+        ))}
     </Box>
   );
 };
